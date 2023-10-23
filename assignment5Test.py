7c18f843a935a91816bf34cd29ea975e443beeee
[Noah King] <[noah.king@bsu.edu]>
2023-10-22 21:13:42 -0400

    I mean it works :D

---
+++ assignment5Test.py
@@ -0,0 +1,25 @@
+import unittest
+import tkinter as tk
+
+from assignment5Main import calculate
+
+years = tk.IntVar()
+years.set(5)
+annualPercentageRate = tk.DoubleVar()
+annualPercentageRate.set(5)
+price = tk.IntVar()
+price.set(20000)
+downpayment = tk.IntVar()
+downpayment.set(3500)
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.calculator = calculate(years, annualPercentageRate, price, downpayment)
+
+    def test_calculate(self):
+        self.assertEqual(self.calculator, 311.38)
+        
+
+if __name__ == "__main__":
+    unittest.main()
